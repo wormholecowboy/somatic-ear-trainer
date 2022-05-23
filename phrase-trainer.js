504194c17770a4e5c3aa3c8a68f2722a55662afd
wormholecowboy <wormholecowboy@gmail.com>
2022-05-23 08:46:49 -0400

    created all the basic functions and set up a protoype

--- phrase-trainer.js
+++ phrase-trainer.js
@@ -1,21 +1,24 @@
 // TODO: rework all variables
 // TODO: create musicaljs object (will passing as object work?)
 
-const loopA = new Tone.Loop(trigSynthA, '4n');
+let piano = new Instrument({ wave: 'piano', detune: 0 });
 const now = Tone.now();
 let startStopState = false;
-let mjsArray = [];
-let speed;
+let notesArray = ['B', 'C', 'D', 'E', 'F'];
+const rhythmArray = ['', '/2', '/4', '<', '>', '2', '4'];
+let speed = 80;
+let numOfNotes = 5;
+let currentPhrase = '';
+const loop = new Tone.Loop(playPhrase(), '4n');
 
-function run() {
-  greenRedLight();
+function startStop() {
   if (startStopState == true) {
     Tone.Transport.stop();
     startStopState = false;
-    console.log('stop');
+    console.log('Stopped');
   } else {
     startStopState = true;
-    console.log('Start');
+    console.log('Started');
     Tone.start();
     startTransport();
   }
@@ -24,29 +27,43 @@ function run() {
 function startTransport() {
   Tone.Transport.bpm.value = speed;
   Tone.Transport.start();
-  phraseTrainer.start();
+  loop.start();
 }
 
-function randomNotes(min, max) {
+function randomIndex(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
 
-  previousNote = currentNote;
-
-  while (currentNote == previousNote) {
-    currentNote = Math.floor(Math.random() * (max - min) + min);
-  }
-  return currentNote;
+  let val = Math.floor(Math.random() * (max - min) + min);
+  return val;
 }
 
-function random(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-
-  prevRhythm = currentRhythm;
-
-  while (currentRhythm == prevRhythm) {
-    currentRhythm = Math.floor(Math.random() * (max - min) + min);
-  }
-  return currentRhythm;
+function getRandomNote() {
+  i = randomIndex(0, notesArray.length);
+  return notesArray[i];
 }
+
+function getRandomRhythm() {
+  i = randomIndex(0, rhythmArray.length);
+  return rhythmArray[i];
+}
+
+function createPhrase(num) {
+  for (i = 0; i < numOfNotes; i++) {
+    randomNote = () => getRandomNote();
+    randomRhythm = () => getRandomRhythm();
+    currentPhrase = currentPhrase + randomNote + randomRhythm;
+  }
+}
+
+let formattedPhrase = `X:1
+M:4/4
+L:1/4
+Q:80
+K:Bm
+${currentPhrase}`;
+
+function playPhrase() {
+  piano.play(formattedPhrase);
+}
+console.log(formattedPhrase);
