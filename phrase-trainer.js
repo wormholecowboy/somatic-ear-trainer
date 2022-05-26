bc3b4cab76bfb83a0b550be6a46729179f0afb52
wormholecowboy <wormholecowboy@gmail.com>
2022-05-25 22:48:47 -0400

    got the random generator working. so close. just need to find out why loop is not calling its callback.

--- phrase-trainer.js
+++ phrase-trainer.js
@@ -1,26 +1,24 @@
-// TODO: rework all variables
-// TODO: create musicaljs object (will passing as object work?)
-
-let piano = new Instrument({ wave: 'piano', detune: 0 });
-let loop = new Tone.Loop(playPhrase(), '4n');
+//TODO: Tone.js loop is not running.
 //
-let startStopState = false;
-let notesArray = ['B', 'C', 'D', 'E', 'F'];
-const rhythmArray = ['', '/2', '/4', '<', '>', '2', '4'];
 //
-let speed = 80;
 var numOfNotes = 5;
-let currentPhrase = {
+var currentPhrase = {
   current: '',
   setCurrent(val) {
     this.current = val;
   },
 };
+var startStopState = false;
+var notesArray = ['B', 'C', 'D', 'E', 'F'];
+const rhythmArray = ['', '/2', '/4', '2', '4'];
+var speed = 80;
+var piano = new Instrument({ wave: 'piano', detune: 0 });
 
 function startStop() {
   if (startStopState == true) {
     Tone.Transport.stop();
     startStopState = false;
+    currentPhrase.setCurrent('');
     console.log('Stopped');
   } else {
     startStopState = true;
@@ -34,15 +32,16 @@ function startTransport() {
   Tone.Transport.bpm.value = speed;
   Tone.Transport.start();
   loop.start();
+  //piano.tone('D');
+  createPhrase();
+  console.log('formatedphrase', formattedPhrase);
 }
 
 function randomIndex(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
 
-  let val = () => {
-    Math.floor(Math.random() * (max - min) + min);
-  };
+  let val = Math.floor(Math.random() * (max - min) + min);
   return val;
 }
 
@@ -51,16 +50,18 @@ function getRandomNote() {
   return notesArray[i];
 }
 
-function getRandomRhythm() {
+let getRandomRhythm = () => {
   let i = randomIndex(0, rhythmArray.length);
   return rhythmArray[i];
-}
+};
 
 function createPhrase() {
-  for (let i = 0; i < numOfNotes; i++) {
-    randomNote = () => getRandomNote();
-    randomRhythm = () => getRandomRhythm();
+  for (let i = 0; i < numOfNotes - 1; i++) {
+    randomNote = getRandomNote();
+    randomRhythm = getRandomRhythm();
+    console.log('randoms: ', randomNote, randomRhythm);
     currentPhrase.setCurrent(currentPhrase.current + randomNote + randomRhythm);
+    console.log(currentPhrase);
   }
 }
 
@@ -75,5 +76,10 @@ var formattedPhrase = `${currentPhrase.current}`;
 
 function playPhrase() {
   createPhrase();
+  console.log('phrase: ', formattedPhrase);
   piano.play({ tempo: 80 }, formattedPhrase);
 }
+var loop = new Tone.Loop(playPhrase(), '8n');
+
+console.log('something', rhythmArray);
+console.log('other', notesArray);
